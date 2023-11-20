21328a6f6fb67546e113ba192ac5836d54d54d13
guidodavoli <guidodavoli@gmail.com>
2023-11-20 16:01:46 +0100

    oroglobo_parameters.py updated

--- oroglobo_parameters.py
+++ oroglobo_parameters.py
@@ -15,16 +15,20 @@ parameters and parameter groups are defined through python dictionaries
 
 
 paths_in = {
-	"srtm30_data":"data_in/srtm30/"
+	"srtm30_data":"data_in/srtm30/",
+	"model_grids":"data_in/model_grids/",
+	"model_masks":"data_in/model_masks/"
 }
 
 paths_out = {
 	"img_out":"img_out/",
-    "data_out":"data_out/"
+    	"data_out":"data_out/"
 }
 
 files_in = {
-	"netcdf_in_srtm30_global_nan_to_zero":"srtm30_global_nan_to_zero.nc"
+	"netcdf_in_srtm30_global_nan_to_zero":"srtm30_global_nan_to_zero.nc",
+	"netcdf_in_model_grid":"grids_globo_KM078.nc",
+	"netcdf_in_model_mask":"mask_globo_KM078.nc"	
 }
 
 files_out = {
